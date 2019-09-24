1d8e24151997a32b74c4bf4c4ef7b60097654da8
xaviier12 <xavi.120595@gmail.com>
2019-09-24 11:31:06 +0000

    Update menu items in openacademy and add book view in library module

--- technical-training-13.0-03-views/library/__manifest__.py
+++ technical-training-13.0-03-views/library/__manifest__.py
@@ -26,6 +26,7 @@
     # always loaded
     'data':        [
         "security/ir.model.access.csv",
+        "views/book_view.xml",
         "data/library_data.xml",
     ],
     # only loaded in demonstration mode
