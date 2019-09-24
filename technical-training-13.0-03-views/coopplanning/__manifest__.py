e6daa48b069e80b224e2df973afeb9787fb20150
Xavier <xavi.120595@gmail.com>
2019-09-24 10:31:40 +0200

    [ADD] app technical-training-13.0-03-views

---
+++ technical-training-13.0-03-views/coopplanning/__manifest__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+{
+    'name':        "Cooperative Management",
+
+    'summary':
+                   """
+                   Cooperative management
+                   """,
+
+    'description': """
+        Manage a cooperative group 
+    """,
+
+    'author':      "Odoo",
+    'website':     "http://www.odoo.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category':    'Cooperative Planning',
+    'version':     '0.2',
+
+    # any module necessary for this one to work correctly
+    'depends':     ['base'],
+
+    # always loaded
+    'data':        [
+        "security/ir.model.access.csv",
+        "data/coop_data.xml",
+        "data/task_template.xml",
+    ],
+    # only loaded in demonstration mode
+    'demo':        [],
+    'license': 'AGPL-3',
+}
