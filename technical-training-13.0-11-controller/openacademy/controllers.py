cddf67a227962819495cb42ae88d71f88e51cb8d
xaviier12 <xavi.120595@gmail.com>
2019-10-01 13:12:00 +0000

    Update controllers for openacademy app

--- technical-training-13.0-11-controller/openacademy/controllers.py
+++ technical-training-13.0-11-controller/openacademy/controllers.py
@@ -2,14 +2,26 @@ from odoo import http
 
 
 class Session(http.Controller):
-    @http.route('/openacademy', auth='public', website="True")
+    @http.route('/openacademy', auth='public', website=True)
     def openacademy(self):
         return '<h1>Bienvenue dans OpenAcademy</h1>'
+    
+    @http.route('/openacademy/courses', auth="public", website=True)
+    def course_list(self):
+        Courses = http.request.env['openacademy.course']
+        return http.request.render('openacademy.course_list', {
+            'courses': Courses.search([])
+        })
 
-    @http.route('/openacademy/sessions', auth="public", website="True")
-    def index(self):
+    @http.route('/openacademy/sessions', auth="public", website=True)
+    def session_list(self):
         Sessions = http.request.env['openacademy.session']
-        return http.request.render('session.index', {
+        return http.request.render('openacademy.session_list', {
             'sessions': Sessions.search([])
         })
     
+    @http.route('/openacademy/sessions/<model("openacademy.session"):session>/', auth="public", website=True)
+    def session(self, session):
+        return http.request.render('openacademy.session', {
+            'session': session
+        })
