4c5583f4dd893a6337900cdff5fc3a6991642bce
xaviier12 <xavi.120595@gmail.com>
2019-10-01 08:30:33 +0000

    add reportg view in library app

--- technical-training-13.0-10-reports/library/__manifest__.py
+++ technical-training-13.0-10-reports/library/__manifest__.py
@@ -26,6 +26,7 @@
     # always loaded
     'data':        [
         "security/ir.model.access.csv",
+        "report/library_book_report.xml",
         "views/menus.xml",
         "views/author.xml",
         "views/books.xml",
