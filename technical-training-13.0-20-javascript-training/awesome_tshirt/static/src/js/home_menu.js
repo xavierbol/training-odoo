60bf9c395844beb87861a02d48d359ddf77ce55e
Xavier Bol (xbo) <xbo@odoo.com>
2019-10-09 09:26:04 +0200

    Task 2.7.2

--- technical-training-13.0-20-javascript-training/awesome_tshirt/static/src/js/home_menu.js
+++ technical-training-13.0-20-javascript-training/awesome_tshirt/static/src/js/home_menu.js
@@ -2,13 +2,14 @@ odoo.define('awesome_tshirt.HomeMenu', function (require) {
     "use strict";
 
     const HomeMenu = require('web_enterprise.HomeMenu');
+    const session = require('web.session');
 
     HomeMenu.include({
         _render: function () {
             this._super();
             const elem = document.createElement('div');
             elem.className = "alert alert-warning";
-            elem.appendChild(document.createTextNode('Bafien Ckinpaers is watching you!'));
+            elem.appendChild(document.createTextNode(session.home_menu_message));
             this.$el.prepend(elem);
         }
     })
