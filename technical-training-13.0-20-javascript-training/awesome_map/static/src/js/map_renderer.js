8d2afb4e12c769ca0e115a32177e6f05c83f2b1c
Xavier Bol (xbo) <xbo@odoo.com>
2019-10-09 14:19:56 +0200

    Task 3.1 complete

--- technical-training-13.0-20-javascript-training/awesome_map/static/src/js/map_renderer.js
+++ technical-training-13.0-20-javascript-training/awesome_map/static/src/js/map_renderer.js
@@ -7,6 +7,11 @@ const session = require('web.session');
 const utils = require('web.utils');
 
 const MapRenderer = AbstractRenderer.extend({
+    _render: function () {
+        console.log(this)
+        this.$el.text('Hello World');
+        this._super.apply(this, arguments);
+    }
 });
 
 return MapRenderer;
